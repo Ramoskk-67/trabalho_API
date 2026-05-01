a0372b40b6dbb0871e5cff191c7fd2290cc456b0
Luiz <lamasluizmiguel@gmail.com>
2026-05-01 19:17:47 -0300

    Update AtualizaResultadoRequest.java

--- src/main/java/br/com/mensageria/dto/AtualizaResultadoRequest.java
+++ src/main/java/br/com/mensageria/dto/AtualizaResultadoRequest.java
@@ -1,11 +1,13 @@
 package br.com.mensageria.dto;
+
 import br.com.mensageria.entity.Evento;
 import jakarta.validation.constraints.NotNull;
+
 /**
-* PATCH típico: só os campos que o cliente pode mudar nesta operação.
-*/
+ * PATCH típico: só os campos que o cliente pode mudar nesta operação.
+ */
 public record AtualizaResultadoRequest(
-@NotNull(message = "Status é obrigatório")
-Evento.StatusEvento status,
-String resultado // opcional: ex. mensagem de erro ou sucesso
-) {}
+        @NotNull(message = "Status é obrigatório") Evento.StatusEvento status,
+        String resultado // opcional: ex. mensagem de erro ou sucesso
+) {
+}
