5b500c51b36533d7f288aa56d5c6095097ed75b8
Arthur Yuji Mendes Suzuki <yujisaccont@gmail.com>
2026-05-03 17:38:07 -0300

    Update NovoEventoRequest.java

--- src/main/java/br/com/mensageria/dto/NovoEventoRequest.java
+++ src/main/java/br/com/mensageria/dto/NovoEventoRequest.java
@@ -1,9 +1,9 @@
 package br.com.mensageria.dto;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.validation.constraints.NotNull;
 
 public record NovoEventoRequest(
     @NotNull(message = "Payload é obrigatório")
-    JsonNode payload
+    ObjectNode payload
 ) {}
